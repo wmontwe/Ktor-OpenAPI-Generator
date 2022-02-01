607b2dbc48234004b4003dff4ca0a7d58a548a80
Wolf-Martell Montwé <wolf@wolfmontwe.de>
2022-02-01 15:12:31 +0100

    Bump Kotlin 1.5.21 -> 1.5.32, Ktor 1.6.1 -> 1.6.7, Jackson Datatype Jsr310 2.9.8 -> 2.13.1, Logback Classic 1.2.1 -> 1.2.10, Reflections 0.9.11 -> 0.10.2, SLF4J Api 1.7.30 -> 1.7.35

--- build.gradle.kts
+++ build.gradle.kts
@@ -60,6 +60,25 @@ tasks.withType(org.jetbrains.kotlin.gradle.tasks.KotlinCompile::class.java) {
     }
 }
 
+tasks.withType<com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask> {
+    resolutionStrategy {
+        componentSelection {
+            all {
+                if (isNonStable(candidate.version) && !isNonStable(currentVersion)) {
+                    reject("Release candidate")
+                }
+            }
+        }
+    }
+}
+
+fun isNonStable(version: String): Boolean {
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().contains(it) }
+    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+    val isStable = stableKeyword || regex.matches(version)
+    return isStable.not()
+}
+
 tasks.named<Wrapper>("wrapper") {
     gradleVersion = "7.3.3"
 }
